1751bed691c720c1db6c135a97f2543d2052536d
tarikul-islam <tariikul12@gmail.com>
2023-05-01 22:39:41 +0600

    italian recipe project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Route from './routes/Route';
+
+
+const App = () => {
+  return (
+    <div>
+      <Route></Route>
+      <h1 className='text-danger'>hello</h1>
+    </div>
+  );
+};
+
+export default App;
