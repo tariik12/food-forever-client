28d7028f8708b58371ea68dfeee0c66f48d8a019
tarikul-islam <tariikul12@gmail.com>
2023-05-02 01:43:33 +0600

    navber Done

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtvD5kXca1a6cVVV6i9MtFyU4NpyLhiXE",
+  authDomain: "italian-recipe-assignment10.firebaseapp.com",
+  projectId: "italian-recipe-assignment10",
+  storageBucket: "italian-recipe-assignment10.appspot.com",
+  messagingSenderId: "912808143277",
+  appId: "1:912808143277:web:93c8b8f5181b1e4515d924"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Lottie from "lottie-react";
 
 const Banner = () => {
+
+const [useLotti, setLotti] = useState({});
+useEffect(() =>{
+    fetch('http://localhost:5000/lotti')
+    .then(res =>res.json())
+    .then(data=> setLotti(data))
+},[])
+if (!useLotti) return <p>Loading...</p>
     return (
-        <div>
-            <h1>This is banner</h1>
-        </div>
+    
+            <div>
+            <Lottie animationData={useLotti} loop={true} />
+            
+            </div>
+       
     );
 };
 
