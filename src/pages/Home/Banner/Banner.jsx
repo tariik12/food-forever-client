5bc53600d3eb61245794c802efcf99b62e80de32
tarikul-islam <tariikul12@gmail.com>
2023-05-02 12:29:55 +0600

    Banner section Done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,23 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Lottie from "lottie-react";
 
-const Banner = () => {
+import banner from "../../../assets/recipe_animation.json"
+import { Card } from 'react-bootstrap';
 
-const [useLotti, setLotti] = useState({});
-useEffect(() =>{
-    fetch('http://localhost:5000/lotti')
-    .then(res =>res.json())
-    .then(data=> setLotti(data))
-},[])
-if (!useLotti) return <p>Loading...</p>
+const Banner = () => {
     return (
+        <div className='d-flex align-items-center'>
+
+            < Card.Body className='w-50 pe-5'>
+                 <Card.Title className=' '>
+                     <span className='fs-1 fw-bold ' >experience the joy</span> <br />
+                     <span className='fs-1 fw-bold ' > OF <span className='text-info '>ITALIAN CUISINE.</span></span>
+                 </Card.Title>
+                    <Card.Text className='fs-4 mt-2'>
+                    <p className=''>The essence of Italian cooking today is simplicity. One uses the freshest seasonal ingredients and basic cooking techniques to simply enhance the natural flavor of the food.</p>
     
-            <div>
-            <Lottie animationData={useLotti} loop={true} />
-            
-            </div>
-       
-    );
+         </Card.Text>
+         <Card.Link className='text-decoration-none text-info' href="#">more about me »</Card.Link>
+       </Card.Body>
+
+            <Lottie className='' animationData={banner} loop={true} />
+        </div>);
 };
 
 export default Banner;
